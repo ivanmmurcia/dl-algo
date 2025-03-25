8e7aae9afab81887c991f19abdb1703d84481725
Iván M.M <ivmmu96@gmail.com>
2025-03-25 20:30:37 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,40 @@
+// Require our Telegram helper package
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.TOKEN_BOT;
+// Export as an asynchronous function
+// We'll wait until we've responded to the user
+module.exports = async (request, response) => {
+    try {
+        // Create our new bot handler with the token
+        // that the Botfather gave us
+        // Use an environment variable so we don't expose it in our code
+        const bot = new TelegramBot(process.env.TOKEN_BOT);
 
-const bot = new TelegramBot(token, { polling: true });
+        // Retrieve the POST request body that gets sent from Telegram
+        const { body } = request;
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const response = `Hola, ${msg.from.first_name}!`;
+        // Ensure that this is a message being sent
+        if (body.message) {
+            // Retrieve the ID for this chat
+            // and the text that the user sent
+            const { chat: { id }, text } = body.message;
 
-  bot.sendMessage(chatId, response);
-});
+            // Create a message to send back
+            // We can use Markdown inside this
+            const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+            // Send our new message back in Markdown
+            await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+        }
+    }
+    catch(error) {
+        // If there was an error sending our message then we 
+        // can log it into the Vercel console
+        console.error('Error sending message');
+        console.log(error.toString());
+    }
+    
+    // Acknowledge the message with Telegram
+    // by sending a 200 HTTP status code
+    response.send('OK');
+};
